f10abe842681c06ece5906a8379efa9ce91fd42d
RaniaLs <100304346+RaniaLs@users.noreply.github.com>
2024-01-16 15:18:31 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles.css">
+    <title>Votre Projet</title>
+</head>
+<body>
+    <?php include 'appo.php'; ?>
+    <?php include 'APPOINT.php'; ?>
+    <?php include 'apres_connexion_doct.php'; ?>
+    <?php include 'confirmation_reservation.php'; ?>
+    <?php include 'doc.php'; ?>
+    <?php include 'Espace.php'; ?>
+    <?php include 'logout.php'; ?>
+    <?php include 'page_apres_connexion.php'; ?>
+    <?php include 'page_apres_insc_doct.php'; ?>
+    <?php include 'page_apres_inscription.php'; ?>
+    <?php include 'page_reservation.php'; ?>
+    <?php include 'page.php'; ?>
+    <?php include 'reservation.php'; ?>
+    <?php include 'search.php'; ?>
+    <?php include 'signin.php'; ?>
+    <?php include 'signup.php'; ?>
+    <?php include 'user.php'; ?>
+   
+</body>
+</html>
